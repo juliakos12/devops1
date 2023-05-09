61decf5fcf589a19d59b601a6dc87be21807990c
juliakos12 <99745958+juliakos12@users.noreply.github.com>
2023-05-09 14:12:33 +0300

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
 
+url="http://localhost:80"
+log_file="error_log.txt"
+email="juliakos498@gmail.com"
+
+response_code=$(curl -s -o /dev/null -w "%{http_code}" $url)
+
+if [[ $response_code -lt 200 || $response_code -ge 400 ]]; then
+  echo "$(date): Error - Response code: $response_code" >> $log_file
+fi
